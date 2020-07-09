6e2c0cd0593ad7d88daf0e7128c3ef167a620fa9
Sashalmi Imre <sashalmi.imre@gmail.com>
2020-07-09 23:18:45 +0200

    Initial Commit

---
+++ Andrew/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Andrew
+//
+//  Created by Sashalmi Imre on 2020. 07. 09..
+//  Copyright © 2020. Sashalmi Imre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
